102cd28b7aa03e9f73611c86581479886a99e5eb
masangana <alexmasangana@gmail.com>
2022-08-16 00:44:45 +0300

    Update schema

--- schema.sql
+++ schema.sql
@@ -47,3 +47,7 @@ CREATE TABLE visits (
     date_of_visit DATE
 );
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX visits_animal_id_idx ON visits(animal_id);
+CREATE INDEX visits_vet_id_idx ON visits(vet_id);
+CREATE INDEX owners_email_idx ON owners(email);
