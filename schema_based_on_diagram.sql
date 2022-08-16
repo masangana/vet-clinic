8e0d75efa88c7d8ef2d8147cad5abcbb0de2b975
masangana <alexmasangana@gmail.com>
2022-08-17 00:24:18 +0300

    Update data queries

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,32 +1,28 @@
 CREATE TABLE patients (
-    id INT  PRIMARY KEY,
+    id INT PRIMARY KEY,
     name VARCHAR(255),
-    date_of_birth DATE,
-   
+    date_of_birth DATE
 );
 
 CREATE TABLE medical_histories(
-    id INT,
+    id INT PRIMARY KEY,
     admitted_at TIMESTAMP,
-    patient_id INT ,
-    status VARCHAR(255),
-    REFERENCES patients(id) 
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR(255)
 );
 
 CREATE TABLE treatments(
     id INT PRIMARY KEY,
     type VARCHAR(255),
-    name VARCHAR(255),
-    
+    name VARCHAR(255)
 );
 
 CREATE TABLE invoices(
-    id INT  PRIMARY KEY,
+    id INT PRIMARY KEY,
     total_amount DECIMAL,
     generated_at TIMESTAMP,
     payed_at TIMESTAMP,
-    medical_history_id INT,
-    REFERENCES medical_histories(id)
+    medical_history_id INT REFERENCES medical_histories(id)
 );
 
 CREATE TABLE invoice_items(
@@ -34,21 +30,14 @@ CREATE TABLE invoice_items(
     unit_price DECIMAL,
     quantity INT,
     total_price DECIMAL,
-    invoice_id INT,
-    treatment_id INT ,
-    
-    REFERENCES invoices(id),
-   REFERENCES treatments(id)
+    invoice_id INT REFERENCES invoices(id),
+    treatment_id INT REFERENCES treatments(id)
 );
 
-
-CREATE TABLE medical_histories_treatments (
-    
+CREATE TABLE medical_histories_treatments (  
     medical_histories_id INT,
     treatment_id INT,
-    CONSTRAINT fk_medical-treatment FOREIGN KEY(medical_histories_id) 
+    CONSTRAINT fk_medical_treatment FOREIGN KEY(medical_histories_id) 
     REFERENCES medical_histories (id),
-    CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatment(id)
-    
-
+    CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 )
